f7db037293a51b17cb7d085b5be898acdfae3c76
HappyPinkyButterfly <aljaz.gim@gmail.com>
2025-05-29 22:00:30 +0200

    _

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,11 +1,26 @@
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
     private bool isCompetitiveMode;
+    public Sprite start;
+    public Sprite compStart;
+
+    public Image startButton;
     public void Start()
     {
         isCompetitiveMode = Prefrences.Instance.competitiveOn;
+
+        if (!isCompetitiveMode )
+        {
+            startButton.sprite = start;
+        }
+        else
+        {
+            startButton.sprite = compStart;
+        }
     }
 }
   
